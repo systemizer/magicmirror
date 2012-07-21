62f2b146cd04b360cfb282432876f92441349a94
Rob McQueen <robmcqn@gmail.com>
2012-07-21 15:52:29 -0700

    added wikipedia request handler hello world

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import urllib
+import requests
+import json
 import tornado.web
 import tornado.ioloop
 from tornado import template, ioloop
@@ -9,8 +12,24 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write(loader.load("index.html").generate())
 
+class WikipediaHandler(tornado.web.RequestHandler):
+    def get(self):
+        keyword = self.get_argument("keyword")
+
+        payload = {'format':'json',
+                   'action':'query',
+                   'titles':keyword,
+                   'prop':'revisions',
+                   'rvprop':'content'}        
+
+        base_url = "http://en.wikipedia.org/w/api.php"
+        url = "%s?%s" % (base_url,urllib.urlencode(payload))
+        r = requests.get(url)
+        self.write(r.json)
+
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/resources/wikipedia/", WikipediaHandler),
     (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': "static"}),
 ])
 
